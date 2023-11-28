6dd4067027911d67ce36e9e3de7c9ebf73e3c35a
Jaqb-Janicqi <janicus321@gmail.com>
2023-11-28 15:22:58 +0100

    Base classes for the program

---
+++ base_classes.py
@@ -0,0 +1,19 @@
+class User():
+    def __init__(self, name, surname, age, email, password, weight, height):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.email = email
+        self.password = password
+        self.weight = weight
+        self.height = height
+        
+class Skill():
+    def __init__(self, name, manufacturer, proficiency, stiffness, length, width):
+        self.name = name
+        self.manufacturer = manufacturer
+        self.proficiency = proficiency
+        self.stiffness = stiffness
+        self.length = length
+        self.width = width
+    
