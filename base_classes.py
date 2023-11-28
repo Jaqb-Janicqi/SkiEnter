30b7729d6d98cc8795a1fcc2f1008f5cbe6b05fc
Jaqb-Janicqi <janicus321@gmail.com>
2023-11-28 16:17:47 +0100

    user attributes

--- base_classes.py
+++ base_classes.py
@@ -1,5 +1,5 @@
 class User():
-    def __init__(self, name, surname, age, email, password, weight, height):
+    def __init__(self, name, surname, age, email, password, weight, height, proficiency):
         self.name = name
         self.surname = surname
         self.age = age
@@ -7,7 +7,24 @@ class User():
         self.password = password
         self.weight = weight
         self.height = height
-        
+        self.proficiency = proficiency
+
+    @property
+    def bmi(self):
+        return self.weight / (self.height ** 2)
+
+    @property
+    def ski_length(self):
+        return self.height * 0.95
+
+
+class SkiPreference():
+    def __init__(self, user, stiffness, width):
+        self.user = user
+        self.stiffness = stiffness
+        self.width = width
+
+
 class Ski():
     def __init__(self, name, manufacturer, proficiency, stiffness, length, width, ski_number):
         self.name = name
@@ -17,4 +34,3 @@ class Ski():
         self.length = length
         self.width = width
         self.ski_number = ski_number
-    
